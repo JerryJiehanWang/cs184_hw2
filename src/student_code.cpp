a618caba7e57b940bb9df993e6052bb5db079a79
Jiehan Wang <jiehanwang@berkeley.edu>
2019-02-18 22:50:35 -0800

    finish part 4

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -70,10 +70,10 @@ namespace CGL
 
   Vector3D Vertex::normal( void ) const
   {
-    // TODO Part 3.
-    // TODO Returns an approximate unit normal at this vertex, computed by
-    // TODO taking the area-weighted average of the normals of neighboring
-    // TODO triangles, then normalizing.
+    // Part 3.
+    // Returns an approximate unit normal at this vertex, computed by
+    // taking the area-weighted average of the normals of neighboring
+    // triangles, then normalizing.
     Vector3D n(0,0,0);
     HalfedgeCIter h = halfedge(); //get the half Edge corresponding to this vertex.
     h = h -> twin();
@@ -89,17 +89,113 @@ namespace CGL
 
   EdgeIter HalfedgeMesh::flipEdge( EdgeIter e0 )
   {
-    // TODO Part 4.
-    // TODO This method should flip the given edge and return an iterator to the flipped edge.
-    return e0;
+    // Part 4.
+    // This method should flip the given edge and return an iterator to the flipped edge.
+    if (e0 -> isBoundary()) { //return immediately if this is a boundary edge.
+        return e0;
+    } else {
+        HalfedgeIter h0 = e0 -> halfedge();
+        HalfedgeIter h1 = h0 -> next();
+        HalfedgeIter h2 = h1 -> next();
+        HalfedgeIter h3 = h0 -> twin();
+        HalfedgeIter h4 = h3 -> next();
+        HalfedgeIter h5 = h4 -> next();
+        HalfedgeIter h6 = h1 -> twin();
+        HalfedgeIter h7 = h2 -> twin();
+        HalfedgeIter h8 = h4 -> twin();
+        HalfedgeIter h9 = h5 -> twin();
+
+
+        //all vertices
+        //e0 is already given to us.
+        VertexIter v0 = h0 -> vertex();
+        VertexIter v1 = h3 -> vertex();
+        VertexIter v2 = h2 -> vertex();
+        VertexIter v3 = h5 -> vertex();
+
+        //all edges
+        EdgeIter e1 = h1 -> edge();
+        EdgeIter e2 = h2 -> edge();
+        EdgeIter e3 = h4 -> edge();
+        EdgeIter e4 = h5 -> edge();
+
+        //all faces
+        FaceIter f0 = h0 -> face();
+        FaceIter f1 = h3 -> face();
+
+        //handle the changed half edges
+        h0 -> setNeighbors(h1, h3, v3, e0, f0);
+        h1 -> setNeighbors(h2, h7, v2, e2, f0);
+        h2 -> setNeighbors(h0, h8, v0, e3, f0);
+        h3 -> setNeighbors(h4, h0, v2, e0, f1);
+        h4 -> setNeighbors(h5, h9, v3, e4, f1);
+        h5 -> setNeighbors(h3, h6, v1, e1, f1);
+
+        //outside half edges
+        h6 -> setNeighbors(h6 -> next(), h5, v2, e1, h6 -> face());
+        h7 -> setNeighbors(h7 -> next(), h1, v0, e2, h7 -> face());
+        h8 -> setNeighbors(h8 -> next(), h2, v3, e3, h8 -> face());
+        h9 -> setNeighbors(h9 -> next(), h4, v1, e4, h9 -> face());
+
+        //vertices
+        v0 -> halfedge() = h2;
+        v1 -> halfedge() = h5;
+        v2 -> halfedge() = h3;
+        v3 -> halfedge() = h0;
+
+        //handle edges.
+        e0 -> halfedge() = h0;
+        e1 -> halfedge() = h5;
+        e2 -> halfedge() = h1;
+        e3 -> halfedge() = h2;
+        e4 -> halfedge() = h4;
+
+        //handle faces
+        f0 -> halfedge() = h0;
+        f1 -> halfedge() = h3;
+
+        return e0;
+    }
   }
 
   VertexIter HalfedgeMesh::splitEdge( EdgeIter e0 )
   {
-    // TODO Part 5.
-    // TODO This method should split the given edge and return an iterator to the newly inserted vertex.
-    // TODO The halfedge of this vertex should point along the edge that was split, rather than the new edges.
-    return newVertex();
+    // Part 5.
+    // This method should split the given edge and return an iterator to the newly inserted vertex.
+    // The halfedge of this vertex should point along the edge that was split, rather than the new edges.
+      if (e0 -> isBoundary()) { //return immediately if this is a boundary edge.
+          return newVertex();
+      } else {
+          HalfedgeIter h0 = e0 -> halfedge();
+          HalfedgeIter h1 = h0 -> next();
+          HalfedgeIter h2 = h1 -> next();
+          HalfedgeIter h3 = h0 -> twin();
+          HalfedgeIter h4 = h3 -> next();
+          HalfedgeIter h5 = h4 -> next();
+          HalfedgeIter h6 = h1 -> twin();
+          HalfedgeIter h7 = h2 -> twin();
+          HalfedgeIter h8 = h4 -> twin();
+          HalfedgeIter h9 = h5 -> twin();
+
+
+          //all vertices
+          //e0 is already given to us.
+          VertexIter v0 = h0 -> vertex();
+          VertexIter v1 = h3 -> vertex();
+          VertexIter v2 = h2 -> vertex();
+          VertexIter v3 = h5 -> vertex();
+
+          //all edges
+          EdgeIter e1 = h1 -> edge();
+          EdgeIter e2 = h2 -> edge();
+          EdgeIter e3 = h4 -> edge();
+          EdgeIter e4 = h5 -> edge();
+
+          //all faces
+          FaceIter f0 = h0 -> face();
+          FaceIter f1 = h3 -> face();
+      }
+      return newVertex();
   }
 
 
